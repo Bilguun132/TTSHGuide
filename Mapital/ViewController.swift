705451ab3fd8eb7ec81adfc62ba0472cc397ad22
Bilguun <bilguun132@gmail.com>
2019-04-18 10:04:56 +0800

    Initial Commit

---
+++ Mapital/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mapital
+//
+//  Created by Bilguun Batbold on 18/4/19.
+//  Copyright © 2019 Bilguun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
